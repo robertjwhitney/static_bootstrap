502263570235fbb8e311c32807b105c122484870
robertjwhitney <robertj.whitney@gmail.com>
2011-05-03 17:44:46 -0400

    adds root helpers and site config

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,2 +1,10 @@
 include NanocFuel::Helpers::Facebook
 include NanocFuel::Helpers::Google
+
+def root_url
+  "//#{@site.config[:host]}#{@site.config[:path]}#{@site.config[:project]}"
+end
+
+def root_path
+  "#{@site.config[:path]}#{@site.config[:project]}"
+end
